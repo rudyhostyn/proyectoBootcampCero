c7edc05b481c40aa8cbeb3349386f5fc2d61fc2e
rudyhostyn <68102881+rudyhostyn@users.noreply.github.com>
2021-07-26 17:26:58 +0200

    pegado por error

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 DATABASE = "dbmovimientos.db"
 API_COINMARKET = "Introduce la clave que obtienes en https://coinmarketcap.com/api/"
-git statusIP_ADRESS = "introduce la dirección de acesso. Por ejemplo 'localhost:5000'"
+IP_ADRESS = "introduce la dirección de acesso. Por ejemplo 'localhost:5000'"
